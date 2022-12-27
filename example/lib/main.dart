fb55873459b069eb986e400fdf68273ecd769371
Diney <dineydm@gmail.com>
2022-12-27 16:28:36 -0300

    change example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:dm_stepper/dm_stepper.dart';
 
-import 'dm_step_icon.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -86,4 +84,67 @@ List<DMStepModel> getDMStepList() {
       dmStepType: DMStepType.next,
     ),
   ];
+}
+
+class DMStepIcon extends StatelessWidget {
+  const DMStepIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: 5,
+                width: 5,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(1),
+                ),
+              ),
+              const SizedBox(width: 1.5),
+              Container(
+                height: 5,
+                width: 5,
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(147, 154, 230, 1),
+                  borderRadius: BorderRadius.circular(1),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 1.5),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: 5,
+                width: 5,
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(147, 154, 230, 1),
+                  borderRadius: BorderRadius.circular(1),
+                ),
+              ),
+              const SizedBox(width: 1.5),
+              Container(
+                height: 5,
+                width: 5,
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(147, 154, 230, 1),
+                  borderRadius: BorderRadius.circular(1),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
